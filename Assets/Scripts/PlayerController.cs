6831963ebc24c9b32201798210f3a6f596234791
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2019-12-05 12:53:36 +0100

    Prototype 3 is klaar.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour 
 {
     private Rigidbody playerRb;
-    public float jumpForce = 10;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public float jumpForce = 14;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
@@ -14,24 +20,35 @@ public class PlayerController : MonoBehaviour
     void Start () 
     {
         playerRb = GetComponent<Rigidbody> ();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update () 
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstackle")){
             gameOver = true;
-            Debug.Log("Game Over"); 
+            Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
